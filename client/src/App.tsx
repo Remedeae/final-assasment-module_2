5aa8da9acb6308f9f85b9cadfc9aa51c7a3d49fc
Remedeae <sanna.m.blomdahl@gmail.com>
2025-10-25 19:50:21 +0200

    made the game page to a single page that renders picture and titel depending on API call. Added a get function for the games based on id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,7 @@ import Users from "./Pages/Users";
 import SignUp from "./Pages/SignUp";
 import UserStats from "./Pages/UserStats";
 import Games from "./Pages/Games";
-import Game1 from "./Pages/games/Game1";
-import Game2 from "./Pages/games/Game2";
-import Game3 from "./Pages/games/Game3";
-import Game4 from "./Pages/games/Game4";
+import Game from "./Pages/Game";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 
@@ -20,11 +17,8 @@ function App() {
           <Route path="/" element={<SignUp />} />
           <Route path="/users" element={<Users />} />
           <Route path="/user/:id" element={<UserStats />} />
-          <Route path="/games" element={<Games />} />{" "}
-          <Route path="/game1" element={<Game1 />} />
-          <Route path="/game2" element={<Game2 />} />
-          <Route path="/game3" element={<Game3 />} />
-          <Route path="/game4" element={<Game4 />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/game/:id" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </>
